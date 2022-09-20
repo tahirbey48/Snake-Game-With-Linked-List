99b70a6e07bf37c53d5a3a7211ba221db9a72918
tahirbey48 <itkokten@gmail.com>
2022-09-20 20:55:36 +0300

    head is moving smoothly w/ rotations

--- Assets/Main.cs
+++ Assets/Main.cs
@@ -1,195 +1,135 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using static UnityEngine.GraphicsBuffer;
+
 public class Main : MonoBehaviour
 {
 
 
-    public GameObject border1;
-    public GameObject border2;
-    public GameObject border3;
-    public GameObject border4;
-    //public Text myText;
-    float speed = 10;
-    CubeList cubeList;
+    private CubeList cubeList;
+    [SerializeField] float speed = 10;
+    [SerializeField] GameObject headCube;
+    private Vector3 moveDirection = Vector3.forward;
+
     int i = 0;
     private void Awake()
     {
-        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        cube.transform.localPosition = new Vector3(0, 1, 0);
-        //cube.transform.localScale = new Vector3(1, 1, 1);
-        //cube.GetComponent<MeshRenderer>().material.color = Color.red;
-        cubeList = new CubeList(0, cube);
-        //cubeList.append_cubes(0,cube);
-
-        //myText.text = "asd";
-        
-
+        cubeList = new CubeList(0, headCube);
     }
     void Start()
     {
 
     }
 
-    // Update is called once per frame
     void Update()
     {
-        float Xdir = Input.GetAxis("Horizontal");
-        float Ydir = Input.GetAxis("Vertical");
-        //float Zdir = Input.GetAxis("")
-        //transform.Translate(Vector3.right * Xdir * speed * Time.deltaTime);
-        //transform.Translate(Vector3.up * Ydir * speed * Time.deltaTime);
-        Vector3 direction = new Vector3(Xdir,Ydir,0);
-        transform.Translate(direction * speed * Time.deltaTime);
 
-        if (Input.GetMouseButtonDown(0))
+        MoveHeadOfLinkedList();
+        if (Input.GetKeyDown("c"))
         {
-            int ii = Random.Range(1, 30);
-            int jj = Random.Range(1, 30);
 
-
-            
             GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
             GameObject myText = new GameObject();
-            cube.transform.localPosition = new Vector3(jj, ii, 0);
-            myText.transform.localPosition = new Vector3(jj, ii, 0);
-            cubeList.append_cube(ii, cube);
-            
+            cube.transform.localPosition = new Vector3(0, 0, 0);
+            cubeList.append_cube(3, cube);
+
             myText.transform.SetParent(cube.transform);
             TextMesh textMesh = myText.AddComponent<TextMesh>();
-            textMesh.text = ii.ToString();
+            textMesh.text = 3.ToString();
             textMesh.color = Color.black;
-
-
         }
 
+
+
+
+        TranslationMethodForTail();
+
+
+    }
+
+    private void MoveHeadOfLinkedList()
+    {
+        cubeList.head.body.transform.position = Vector3.MoveTowards(cubeList.head.body.transform.position, new Vector3(cubeList.head.body.transform.position.x, cubeList.head.body.transform.position.y, cubeList.head.body.transform.position.z) + moveDirection, 0.1f);
 
         if (Input.GetMouseButtonDown(1))
         {
-            int i = Random.Range(1, 10);
-            delete_gameobject(cubeList, i);
-
-
+            float a = moveDirection.x;
+            float b = moveDirection.y;
+            float c = moveDirection.z;
+            moveDirection = new Vector3(-c, b, a);
         }
 
-        cubeList.head.body.transform.Translate(direction * speed * Time.deltaTime);
-
-        //if (cubeList.head.body.transform.hasChanged)
-        if (Input.anyKey)
+        if (Input.GetMouseButtonDown(0))
         {
-            Cube tmp = cubeList.head;
-            Cube after = tmp.next;
-            while (after != null)
+            float a = moveDirection.x;
+            float b = moveDirection.y;
+            float c = moveDirection.z;
+            moveDirection = new Vector3(c, b, -a);
+        }
+    }
+
+    private void TranslationMethodForTail()
+    {
+        
+        Cube tmp = cubeList.head;
+        if (cubeList.head.next != null)
+        {
+            Cube follower = tmp.next;
+            
+            while (follower != null)   
             {
-                after.body.transform.position = Vector3.MoveTowards(after.body.transform.position, tmp.body.transform.position, 0.1f);
+                if (follower == null)
+                    break;
+                if (tmp == null)
+                    break;
+                if (Vector3.Distance(follower.body.transform.position, tmp.body.transform.position) > 3)
+                { 
+                //Debug.Log("distance     *****   " + Vector3.Distance(after.body.transform.position, tmp.body.transform.position));
+                //after.body.transform.position = Vector3.MoveTowards(after.body.transform.position , tmp.body.transform.position - new Vector3(2,0,2), 0.1f);
+
+                //after.body.transform.position = Vector3.MoveTowards(after.body.transform.position , (tmp.body.transform.position - (after.body.transform.position - transform.position).normalized) * -1f, 0.1f);
+                follower.body.transform.position = Vector3.MoveTowards(follower.body.transform.position,  tmp.body.transform.position, 0.1f);
+                //transform.position = (after.body.transform.position - transform.position).normalized * 10;
+
                 tmp = tmp.next;
-                after = after.next;
+                follower = follower.next;
+                }
+                
             }
         }
-
+            
+            
         
-
-
-
-        //cubeList.head.body.transform.position = Vector3.MoveTowards(cubeList.head.body.transform.position, border1.transform.position, 0.1f);
-        //    Cube tmp = cubeList.head;
-        //    Cube after = tmp.next;
-        //    while (after != null)
-        //    {
-        //        after.body.transform.position = Vector3.MoveTowards(after.body.transform.position, tmp.body.transform.position, 0.1f);
-        //        tmp = tmp.next;
-        //        after = after.next;
-        //    }
-
-
-
-        //if (Input.GetKey("s"))
-        //{
-        //    cubeList.head.body.transform.position = Vector3.MoveTowards(cubeList.head.body.transform.position, border1.transform.position, 0.1f);
-        //    Cube tmp = cubeList.head;
-        //    Cube after = tmp.next;
-        //    while (after != null)
-        //    {
-        //        after.body.transform.position = Vector3.MoveTowards(after.body.transform.position, tmp.body.transform.position, 0.1f);
-        //        tmp = tmp.next;
-        //        after = after.next;
-        //    }
-
-        //}
-
-
-        //if (Input.GetKey("a"))
-        //{
-        //    cubeList.head.body.transform.position = Vector3.MoveTowards(cubeList.head.body.transform.position, border4.transform.position, 0.1f);
-        //    Cube tmp = cubeList.head;
-        //    Cube after = tmp.next;
-        //    while (after != null)
-        //    {
-        //        after.body.transform.position = Vector3.MoveTowards(after.body.transform.position, tmp.body.transform.position, 0.1f);
-        //        tmp = tmp.next;
-        //        after = after.next;
-        //    }
-        //}
-
-
-
-        //if (Input.GetKey("q"))
-        //{
-        //    cubeList.head.body.transform.position = Vector3.MoveTowards(cubeList.head.body.transform.position, border3.transform.position, 0.1f);
-        //    Cube tmp = cubeList.head;
-        //    Cube after = tmp.next;
-        //    while (after != null)
-        //    {
-        //        after.body.transform.position = Vector3.MoveTowards(after.body.transform.position, tmp.body.transform.position, 0.1f);
-        //        tmp = tmp.next;
-        //        after = after.next;
-        //    }
-        //}
-
-
-
-        //if (Input.GetKey("w"))
-        //{
-        //    cubeList.head.body.transform.position = Vector3.MoveTowards(cubeList.head.body.transform.position, border2.transform.position, 0.1f);
-        //    Cube tmp = cubeList.head;
-        //    Cube after = tmp.next;
-        //    while (after != null)
-        //    {
-        //        after.body.transform.position = Vector3.MoveTowards(after.body.transform.position, tmp.body.transform.position, 0.1f);
-        //        tmp = tmp.next;
-        //        after = after.next;
-        //    }
-        //}
-
-
-        //if (Input.GetKey("d"))
-        //{
-        //    int ind = Mathf.RoundToInt(i / 3); //index to delete
-        //    Debug.Log("index to delete    " + ind);           
-        //    //Cube before = cubeList.get_cube(ind-1);
-        //    //Cube delete = before.next;
-        //    //before.next = delete.next;
-        //    //delete.next = null;
-        //    Cube tmp = cubeList.get_cube(ind-1);
-        //    Cube cubeToDelete = tmp.next;
-        //    Cube afterDelete = cubeList.get_cube(ind + 1);
-        //    cubeList.remove_cube(ind);
-        //    Destroy(cubeToDelete.body);
-        //    tmp.next = afterDelete;
-
-
-
-        //}
-
-        //if (Input.GetKey("p"))
-        //{
-        //    Debug.Log("p pressed");
-        //    cubeList.pop_cube();
-        //    Destroy(cubeList.tail.body);
-        //}
-        refreshColor(cubeList);
+            
+        
     }
+
+
+    // private void TranslationMethodForTail1()
+    // {
+        
+    //         Cube tmp = cubeList.head;
+    //         Cube after = tmp.next;
+            
+    //         while (after != null)
+    //         {
+    //             if(!(2 < Vector3.Distance(after.body.transform.position, tmp.body.transform.position) &&  Vector3.Distance(after.body.transform.position, tmp.body.transform.position) < 5))
+    //             {
+    //                 //Debug.Log("distance     *****   " + Vector3.Distance(after.body.transform.position, tmp.body.transform.position));
+    //                 after.body.transform.position = Vector3.MoveTowards(after.body.transform.position, tmp.body.transform.position, 0.1f);
+    //                 tmp = tmp.next;
+    //                 after = after.next;
+    //             }
+                
+    //         }
+            
+        
+    // }
+
+
 
 
 
