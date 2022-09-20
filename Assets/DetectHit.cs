4fc6911506e2dbe420f75dac4b769d77997ac645
tahirbey48 <itkokten@gmail.com>
2022-09-20 21:57:03 +0300

    tail movements and collision detection on walls are done

---
+++ Assets/DetectHit.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectHit : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Head")
+        {
+            Debug.Log("Hit Detected .... ");
+
+        }
+    }
+}
--- Assets/Main.cs
+++ Assets/Main.cs
@@ -28,6 +28,14 @@ public class Main : MonoBehaviour
     {
 
         MoveHeadOfLinkedList();
+        AppendCubes();
+        TranslationMethodForTail();
+
+
+    }
+
+    private void AppendCubes()
+    {
         if (Input.GetKeyDown("c"))
         {
 
@@ -41,20 +49,13 @@ public class Main : MonoBehaviour
             textMesh.text = 3.ToString();
             textMesh.color = Color.black;
         }
-
-
-
-
-        TranslationMethodForTail();
-
-
     }
 
     private void MoveHeadOfLinkedList()
     {
         cubeList.head.body.transform.position = Vector3.MoveTowards(cubeList.head.body.transform.position, new Vector3(cubeList.head.body.transform.position.x, cubeList.head.body.transform.position.y, cubeList.head.body.transform.position.z) + moveDirection, 0.1f);
 
-        if (Input.GetMouseButtonDown(1))
+        if (Input.GetKeyDown("a"))
         {
             float a = moveDirection.x;
             float b = moveDirection.y;
@@ -62,7 +63,7 @@ public class Main : MonoBehaviour
             moveDirection = new Vector3(-c, b, a);
         }
 
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetKeyDown("d"))
         {
             float a = moveDirection.x;
             float b = moveDirection.y;
@@ -81,65 +82,15 @@ public class Main : MonoBehaviour
             
             while (follower != null)   
             {
-                if (follower == null)
-                    break;
-                if (tmp == null)
-                    break;
-                if (Vector3.Distance(follower.body.transform.position, tmp.body.transform.position) > 3)
+                if (Vector3.Distance(follower.body.transform.position, tmp.body.transform.position) > 1.2)
                 { 
-                //Debug.Log("distance     *****   " + Vector3.Distance(after.body.transform.position, tmp.body.transform.position));
-                //after.body.transform.position = Vector3.MoveTowards(after.body.transform.position , tmp.body.transform.position - new Vector3(2,0,2), 0.1f);
-
-                //after.body.transform.position = Vector3.MoveTowards(after.body.transform.position , (tmp.body.transform.position - (after.body.transform.position - transform.position).normalized) * -1f, 0.1f);
                 follower.body.transform.position = Vector3.MoveTowards(follower.body.transform.position,  tmp.body.transform.position, 0.1f);
-                //transform.position = (after.body.transform.position - transform.position).normalized * 10;
-
-                tmp = tmp.next;
-                follower = follower.next;
                 }
-                
+                tmp = tmp.next;
+                follower = follower.next;  
             }
-        }
-            
-            
-        
-            
-        
+        }           
     }
-
-
-    // private void TranslationMethodForTail1()
-    // {
-        
-    //         Cube tmp = cubeList.head;
-    //         Cube after = tmp.next;
-            
-    //         while (after != null)
-    //         {
-    //             if(!(2 < Vector3.Distance(after.body.transform.position, tmp.body.transform.position) &&  Vector3.Distance(after.body.transform.position, tmp.body.transform.position) < 5))
-    //             {
-    //                 //Debug.Log("distance     *****   " + Vector3.Distance(after.body.transform.position, tmp.body.transform.position));
-    //                 after.body.transform.position = Vector3.MoveTowards(after.body.transform.position, tmp.body.transform.position, 0.1f);
-    //                 tmp = tmp.next;
-    //                 after = after.next;
-    //             }
-                
-    //         }
-            
-        
-    // }
-
-
-
-
-
-    //This code will be used for movement and animations if necessary.
-    //cube.transform.DOMove(border1.transform.position, 1).OnComplete(
-    //           () =>
-    //           {
-    //};
-
-
 
 
 
